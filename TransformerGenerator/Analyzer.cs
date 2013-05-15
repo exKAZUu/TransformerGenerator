ff1d49708aa248cfa37ca5d24321a3ffb6ef9c2b
Kazunori Sakamoto <exkazuu@gmail.com>
2013-05-15 16:27:50 +0900

    Add initial files.

---
+++ TransformerGenerator/Analyzer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace TransformerGenerator {
+    public class DiffierentLine {
+        public string Line { get; set; }
+        public int LineNumber { get; set; }
+    }
+
+    public static class Analyzer {
+        public static List<DiffierentLine> Analyze(string original, string modified) {
+            var originalLines = original.Split('\n');
+            var modifiedLines = modified.Split('\n');
+            var diffLines = new List<DiffierentLine>();
+            for (int i = 0; i < originalLines.Length; i++) {
+                if (i + diffLines.Count >= modifiedLines.Length) {
+                    throw new Exception();
+                }
+                if (originalLines[i] != modifiedLines[i + diffLines.Count]) {
+                    diffLines.Add(new DiffierentLine {
+                            LineNumber = i + diffLines.Count + 1,
+                            Line = modifiedLines[i + diffLines.Count].Trim(),
+                    });
+                }
+            }
+            return diffLines;
+        }
+
+        public static int AnalyzeLineNumber(XElement elem) {
+            foreach (var e in elem.DescendantsAndSelf()) {
+                var attr = e.Attribute("startline");
+                if (attr != null) {
+                    return int.Parse(attr.Value);
+                }
+            }
+            throw new Exception();
+        }
+
+        public static List<XElement> Find(XElement modified, List<DiffierentLine> diffLines) {
+            var iLines = 0;
+            var ret = new List<XElement>();
+            foreach (var elem in modified.DescendantsAndSelf()) {
+                if (elem.Value == diffLines[iLines].Line
+                        && AnalyzeLineNumber(elem) == diffLines[iLines].LineNumber) {
+                    ret.Add(elem);
+                    iLines++;
+                    if (iLines == diffLines.Count) {
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ TransformerGenerator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Code2Xml.Languages.Java.CodeToXmls;
+using Paraiba.Core;
+
+namespace TransformerGenerator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            textBox1.Text =
+                    @"class Test {
+  void main() {
+    int a = 0;
+    a++;
+  }
+}";
+            textBox2.Text =
+                    @"class Test {
+  void main() {
+    stmt();
+    int a = 0;
+    stmt();
+    a++;
+  }
+}";
+        }
+
+        private void RemoveAttributes(XElement elem) {
+            foreach (var e in elem.DescendantsAndSelf()) {
+                e.RemoveAttributes();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var xml1 = JavaCodeToXml.Instance.Generate(textBox1.Text);
+            var xml2 = JavaCodeToXml.Instance.Generate(textBox2.Text);
+            //RemoveAttributes(xml1);
+            //RemoveAttributes(xml2);
+            textBox3.Text = xml1.ToString();
+            textBox4.Text = xml2.ToString();
+            File.WriteAllText("file1.xml", textBox3.Text);
+            File.WriteAllText("file2.xml", textBox4.Text);
+            var processorPath = "java";
+            var arguments = new[] {
+                    "-Djava.ext.dirs=jar",
+                    "-cp",
+                    "jar/xmldiff.jar",
+                    "fc.xml.diff.Diff",
+                    "file1.xml",
+                    "file2.xml",
+                    "diff.xml"
+            };
+            var info = new ProcessStartInfo {
+                    FileName = processorPath,
+                    Arguments = arguments.JoinString(" "),
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    //WorkingDirectory = WorkingDirectory,
+            };
+            using (var p = Process.Start(info)) p.WaitForExit();
+
+            var extractedElement = "nil";
+            var modified = new XElement("stmt");
+            var difflines = Analyzer.Analyze(textBox1.Text, textBox2.Text);
+            var extractedElementList = Analyzer.Find(xml2, difflines);
+            extractedElement =
+                    extractedElementList.Select((x) => x.ToString()).Aggregate((x, y) => x + y);
+            textBox5.Text = extractedElement;
+        }
+    }
+}
